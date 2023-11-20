3e26a429d2e62398b0ef1593219f0d50737313d5
JonathanCarvalho39 <jonathan.carvalho@sptech.school>
2023-11-20 00:50:32 -0300

    Validando banco de dados

--- Sprint3-main/Site-Sensorize/site/src/models/usuarioModel.js
+++ Sprint3-main/Site-Sensorize/site/src/models/usuarioModel.js
@@ -1,28 +1,37 @@
-var database = require("../database/config")
+var database = require('../database/config')
 
 function autenticar(email, senha) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
-    var instrucao = `
+  console.log(
+    "ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ",
+    email,
+    senha
+  )
+  var instrucao = `
         SELECT id, nome, email, fk_empresa as empresaId FROM usuario WHERE email = '${email}' AND senha = '${senha}';
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+    `
+  console.log('Executando a instrução SQL: \n' + instrucao)
+  return database.executar(instrucao)
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucao
 function cadastrar(nome, email, senha, token) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, token);
-    
-    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-    //  e na ordem de inserção dos dados.
-    var instrucao = `
+  console.log(
+    "ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():",
+    nome,
+    email,
+    senha
+  )
+
+  // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+  //  e na ordem de inserção dos dados.
+  var instrucao = `
     update representante set senha_acesso = '${senha}', nome_representante = '${nome}', email_acesso = '${email}' where representante_id = '${token}';
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+    `
+  console.log('Executando a instrução SQL: \n' + instrucao)
+  return database.executar(instrucao)
 }
 
 module.exports = {
-    autenticar,
-    cadastrar
-};
+  autenticar,
+  cadastrar
+}
